1279e0db454a3b6c794648ef151fde02eea094a6
ImTobillo <noyatobi03@gmail.com>
2024-10-09 23:28:17 -0300

    Se inizializa con Spring Initializr

---
+++ src/main/java/com/pethero/PetHero/PetHeroApplication.java
@@ -0,0 +1,13 @@
+package com.pethero.PetHero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetHeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pethero/PetHero/PetHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pethero.PetHero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
